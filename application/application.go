ddf794073c37c39caddc582060c795030e4a74ce
wolf1996 <ksgiv37@gmail.com>
2017-10-21 17:11:23 +0300

    Получен хороший код от protoc

---
+++ application/application.go
@@ -0,0 +1,7 @@
+package application
+
+import "github.com/wolf1996/user/server"
+
+func StartApplication(){
+
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/wolf1996/user/application"
+
+func main()  {
+	application.StartApplication()
+}
