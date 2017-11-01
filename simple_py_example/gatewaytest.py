ad5773de1dc9f1d7ba07c88eae4133d82950c46a
wolf1996 <ksgiv37@gmail.com>
2017-11-01 23:17:40 +0300

    финальный демоскрипт

--- simple_py_example/gatewaytest.py
+++ simple_py_example/gatewaytest.py
@@ -2,6 +2,7 @@
 1
 """
 import requests
+import json
 from requests.auth import HTTPBasicAuth
 
 USERLIST = {"simpleUser": "1", "eventOwner": "1",}
@@ -14,13 +15,68 @@ def getNamedSessions():
         sessions[username] = user
     return sessions
 
-def main():
-    sessions = getNamedSessions()
-    for name, session in sessions.items():
+def test_all(test, sessions):
+    for name, user in sessions.items():
+        print("###############################")
         print(name)
-        resp = session.get("http://127.0.0.1:8080/hello")
-        print(resp.text)
+        test(user)
+        print("###############################")
+
+def hello_test(session):
+    resp = session.get("http://127.0.0.1:8080/hello")
+    print(resp.text)
+
+def user_info_test(session):
+    resp = session.get("http://127.0.0.1:8080/user/info")
+    print(resp.text)
+
+def registration(session):
+    resp = session.post("http://127.0.0.1:8080/events/1/register")
+    print(resp.text)
+    return resp
+
+
+def removetest(session, id = 0):
+    print('remove')
+    resp = session.post("http://127.0.0.1:8080/registrations/{}/remove".format(id))
+    print(resp.text)
+
+
+def registrationslist(session, page = ""):
+    print('list')
+    resp = session.get("http://127.0.0.1:8080/user/registrations/{}".format(page))
+    print(resp.text)
+
+def s_user_reg(sessions):
+    sess = sessions['simpleUser']
+    user_info_test(sess)
+    rsp = registration(sess)
+    jsonResp = json.loads(rsp.text)
+    print(jsonResp['id'])
+    user_info_test(sess)
+    registrationslist(sess)
+    registrationslist(sess, "3")
+    removetest(sess, jsonResp['id'])
+    user_info_test(sess)
     
 
+def non_reg_tes():
+    res = requests.get("http://127.0.0.1:8080/events/1")  
+    print(res)
+    print(res.content) 
+    res = requests.get("http://127.0.0.1:8080/registrations/2")  
+    print(res)
+    print(res.content) 
+
+def main():
+    sessions = getNamedSessions()
+    test_all(hello_test, sessions)
+    test_all(user_info_test, sessions) 
+    print('########################')
+    s_user_reg(sessions)
+    print('########################')
+
+
+
 if __name__ == '__main__':
-    main()  
+    main()
